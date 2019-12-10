782d54f754a30ac1838c0a10a86f206e5af5d550
Bdecline <bdeglane@users.noreply.github.com>
2019-12-10 23:13:22 +0100

    :lipstick: Add header styles

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import styles from './nav.module.css'
+
+
 export function Nav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/long-text">Texte long</Link></li>
