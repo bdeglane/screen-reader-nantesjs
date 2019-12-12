2f7a2419449d9a3032c9da7fba1f6b1299502c47
Bdecline <bdeglane@users.noreply.github.com>
2019-12-12 11:49:35 +0100

    Updates

---
+++ precache-manifest.3322595e5a78f024bad788b2d1f4579b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6083122ebafa5df0cece5195ff8b3d6",
+    "url": "/screen-reader-nantesjs/index.html"
+  },
+  {
+    "revision": "92719436bacb114ec1ab",
+    "url": "/screen-reader-nantesjs/static/css/main.d4317465.chunk.css"
+  },
+  {
+    "revision": "ee52b01e35cf755fadb8",
+    "url": "/screen-reader-nantesjs/static/js/2.09b5ac0b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/screen-reader-nantesjs/static/js/2.09b5ac0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "92719436bacb114ec1ab",
+    "url": "/screen-reader-nantesjs/static/js/main.809b04a7.chunk.js"
+  },
+  {
+    "revision": "ee1c5a8817e45d4af069",
+    "url": "/screen-reader-nantesjs/static/js/runtime-main.b62c789a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/screen-reader-nantesjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
