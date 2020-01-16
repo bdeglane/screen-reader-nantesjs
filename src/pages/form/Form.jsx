bb89f8fcdcc8c63119bb5ba7c4d40731c47434eb
Bdecline <bdeglane@users.noreply.github.com>
2020-01-16 11:04:24 +0100

    :sparkles: Add better form example

--- src/pages/form/Form.jsx
+++ src/pages/form/Form.jsx
@@ -30,6 +30,8 @@ export function Form() {
               <form className={styles.form} onSubmit={onsubmit}>
                 <Input type="text" id="surname" name="surname" placeholder="Nom" label="Nom" />
                 <Input type="text" id="name" name="name" placeholder="Prénom" label="Prénom" />
+                <Input type="text" id="id-input" name="name-input" placeholder="placeholder-input" label="label-input" />
+                <Input type="number" id="age" name="age" label="Votre age" />
                 <Input type="email" id="email" name="email" placeholder="Email" label="Email" />
                 <Input type="text" id="phone" name="phone" placeholder="Téléphone" label="Téléphone" />
                 <Input type="url" id="website" name="website" placeholder="Site internet" label="Site internet" />
@@ -57,7 +59,7 @@ export function Form() {
 
                 <div className={styles.formGroup}>
                   <label htmlFor="demande">Votre demande</label>
-                  <textarea id="demande" name="field5" placeholder="Votre message" />
+                  <textarea id="demande" name="demande" placeholder="Votre message" />
                 </div>
 
                 <input type="submit" value="Envoyer votre demande" />
@@ -71,10 +73,12 @@ export function Form() {
                   <legend>Votre identité</legend>
                   <Input type="text" id="surname" name="surname" placeholder="Nom" label="Nom" />
                   <Input type="text" id="name" name="name" placeholder="Prénom" label="Prénom" />
+                  <Input type="text" id="id-input" name="name-input" placeholder="placeholder-input" label="label-input" />
+                  <Input type="number" id="age" name="age" label="Votre age" />
                 </fieldset>
                 <fieldset>
                   <legend>Vos coordonnées</legend>
-                  <Input type="email" id="email" name="email" placeholder="Email" label="Email" />
+                  <Input type="email" id="email" name="nameemail" placeholder="Email" label="Email" />
                   <Input type="text" id="phone" name="phone" placeholder="Téléphone" label="Téléphone" />
                   <Input type="url" id="website" name="website" placeholder="Site internet" label="Site internet" />
                 </fieldset>
@@ -104,7 +108,7 @@ export function Form() {
 
                   <div className={styles.formGroup}>
                     <label htmlFor="demande">Votre demande</label>
-                    <textarea id="demande" name="field5" placeholder="Votre message" />
+                    <textarea id="demande" name="demande" placeholder="Votre message" />
                   </div>
                 </fieldset>
                 <input type="submit" value="Envoyer votre demande" aria-describedby="redirect-to-text" />
